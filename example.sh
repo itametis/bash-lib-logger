15d2cf0969f28261ac0f7fe6396808347d7ea1fd
Davy CLAISSE <davy.claisse@itametis.com>
2019-04-13 09:58:05 +0200

    Change license to APACHE-2.0

--- example.sh
+++ example.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
 #####
-## COPYRIGHT © ITAMETIS - TOUS DROITS RÉSERVÉS
-## Pour plus d'information veuillez contacter : copyright@itametis.com
+# COPYRIGHT © ITAMETIS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #####
+
 set -u pipefail
 
 . ./src/lib-logger.sh
 
 LOG_LEVEL="$LOG_LEVEL_TRACE"
-#LOG_OUTPUT="/tmp/some/folders/my-file.log"
+
+LOG_OUTPUT=""
+LOG_OUTPUT="/tmp/some/folders/my-file.log"
 
 LOGGER_logTrace "This log is written at TRACE level"
 LOGGER_logDebug "This log is written at DEBUG level"
--- src/lib-logger.sh
+++ src/lib-logger.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 #####
-## COPYRIGHT © ITAMETIS - TOUS DROITS RÉSERVÉS
-## Pour plus d'information veuillez contacter : copyright@itametis.com
+# COPYRIGHT © ITAMETIS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #####
 
 set -u pipefail
@@ -114,7 +125,7 @@ LOGGER_log() {
         rmdir "$LOG_OUTPUT"
         touch "$LOG_OUTPUT"
     fi
-    
+
     if [ "$LOG_OUTPUT" != "" ]; then
         echo "$1" >> "$LOG_OUTPUT"
     else
